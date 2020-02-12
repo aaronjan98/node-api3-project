de038871c74293f3bd6de8c33142fb9a3e979833
aaronjan <aaronjan98@gmail.com>
2020-02-12 12:47:15 -0800

    added logger middleware fxn

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} request to ${req.originalUrl}. \n ${new Date()}`);
+  next();
+}
 
 module.exports = server;
