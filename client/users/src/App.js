3ebd378f0a40121021b2cbfc0d5c8c67ab8c6661
aaronjan <aaronjan98@gmail.com>
2020-02-13 16:14:12 -0800

    created UserList component

--- client/users/src/App.js
+++ client/users/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles/App.css';
 
+import UserList from './components/UserList';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <UserList />
     </div>
   );
 }
---
+++ client/users/src/components/UserList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserList = () => {
+    return(
+        <h4>Hi from UserList</h4>
+    );
+}
+
+export default UserList;
